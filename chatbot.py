d39c96d482cc270c1717a3eab460028458d3108b
khushraj17 <khushrajrane@gmail.com>
2026-06-27 17:06:00 +0530

    first chatbot with chathistoty

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id= "Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+chat_history = [
+    SystemMessage(content="you are an Ai expert")
+]
+
+while True :
+    user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == "exit":
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ",result.content)
+
+print("chathistory : \n", chat_history)
